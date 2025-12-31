0cf33b80e4fa70ca61b691780259a39a484d20e9
mrgithaka <mrgithaka@gmail.com>
2025-12-31 09:26:38 +0300

    chore(`src/app`): add global CSS import and data-theme attribute in `layout.tsx`
    Imported `global.css` and added a `data-theme="system"` attribute to the HTML element in `layout.tsx` to prepare for theme-based styling.
    
    This enables consistent global styles and sets up a foundation for implementing light/dark mode support throughout the application.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,16 @@
+import '../styles/global.css';
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>
-        {children}
-      </body>
+    <html
+      lang='en'
+      data-theme='system'
+    >
+      <body>{children}</body>
     </html>
   );
 }
