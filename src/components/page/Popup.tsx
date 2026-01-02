a8936711cd56113b3fb1cb2a993ba3d7090aaab6
mrgithaka <mrgithaka@gmail.com>
2026-01-02 07:04:06 +0300

    feat(`src/components/page`): add popup layout component in `Popup.tsx`
    Added a new Popup component that manages its own trigger state, integrates with aside visibility, and renders popup content within a page layout.
    
    This enables reusable popup interactions in the header while coordinating with aside behavior for a cleaner and more controlled UI experience.

---
+++ src/components/page/Popup.tsx
@@ -0,0 +1,59 @@
+'use client';
+
+import { Header, Main } from '@components/layout';
+import { Icon } from '@components/shared';
+import { useAside, useTrigger } from '@library/hooks';
+import { HeaderStyles, MainStyles } from '@styles/layout';
+import { PageStyles, PopupStyles } from '@styles/page';
+import { ReactNode, useEffect } from 'react';
+
+interface Props {
+  children?: ReactNode;
+}
+
+export default function Popup({ children }: Props) {
+  const { trigger, pullTrigger } = useTrigger();
+
+  const { setHidden } = useAside();
+
+  useEffect(() => {
+    setHidden(trigger);
+    return () => setHidden(false);
+  }, [trigger, setHidden]);
+
+  return (
+    <div className={PopupStyles.Popup}>
+      <div
+        className={PopupStyles.TriggerOff}
+        onClick={() => pullTrigger()}
+      >
+        <Icon
+          name='popup'
+          alt='Popup Icon'
+          size={16}
+          inverted
+        />
+      </div>
+      {trigger && (
+        <div className={PopupStyles.PopupItself}>
+          <div className={PageStyles.Page}>
+            <Header className={HeaderStyles.Page}>
+              <div className={PopupStyles.Nav}></div>
+              <div
+                className={PopupStyles.TriggerOn}
+                onClick={() => pullTrigger()}
+              >
+                <Icon
+                  name='popup'
+                  alt='Popup Icon'
+                  size={16}
+                />
+              </div>
+            </Header>
+            <Main className={MainStyles.Page}>{children}</Main>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
