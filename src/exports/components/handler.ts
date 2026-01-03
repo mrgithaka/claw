f3b40b129cef5c5f1c9a65d8cc9c0c6e29e2f137
mrgithaka <mrgithaka@gmail.com>
2026-01-03 11:37:35 +0300

    refactor(`/src/exports/components`): update import paths in `handler.ts`
    Changed the import paths in `handler.ts` to reflect the new directory structure under `/components/handler`.
    
    This ensures consistency and avoids module resolution errors after moving the handler components.

--- src/exports/components/handler.ts
+++ src/exports/components/handler.ts
@@ -1,5 +1,5 @@
-export { default as Login } from '@/ui/components/handler/Login';
-export { default as Otp } from '@/ui/components/handler/Otp';
-export { default as Recover } from '@/ui/components/handler/Recover';
-export { default as Register } from '@/ui/components/handler/Register';
-export { default as Reset } from '@/ui/components/handler/Reset';
+export { default as Login } from '@/components/handler/Login';
+export { default as Otp } from '@/components/handler/Otp';
+export { default as Recover } from '@/components/handler/Recover';
+export { default as Register } from '@/components/handler/Register';
+export { default as Reset } from '@/components/handler/Reset';
